47db93b6dba400985ea2f6424767d70a6c4aaadf
INEXPLI <61479966+JezJakub@users.noreply.github.com>
2021-02-07 16:16:46 +0100

    Little fix

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ time.sleep(1)
 def click(x, y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(0.01)  # This pauses the script for 0.01 seconds
+    time.sleep(0.05)  # This pauses the script for 0.01 seconds
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 
 click(947, 519)
-time.sleep(20)
+time.sleep(25)
 
 time.sleep(3)
 click(970, 550)
